b2373016f637c1c80498d2f5255982dd42c36edd
Lénaïc Grolleau <lenaic@lenaic.me>
2017-12-05 23:26:05 +0100

    Comment html

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ include 'Database.php';
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<!-- Add a task form -->
 <h1>Ajouter une tâche :</h1>
 <form action="actions.php" method="POST">
     <input type="hidden" name="action" value="add">
@@ -20,6 +21,7 @@ include 'Database.php';
     <input type="text" name="message" id="message" required>
     <button type="submit"><i class="fa fa-plus"></i> Ajouter</button>
 </form>
+<!-- ./Add a task form -->
 <hr>
 <h1>Liste des tâches :</h1>
 <?php
@@ -34,19 +36,23 @@ foreach ($tasks as $task):
             <li>Message : <?= $task['message'] ?></li>
             <li>Créée le : <?= date('d/m/Y - H:i', $task['created_at']) ?></li>
             <li>
+                <!-- Delete button -->
                 <form action="actions.php" method="POST">
                     <input type="hidden" name="action" value="delete">
                     <input type="hidden" name="id" value="<?= $task['id'] ?>">
                     <button type="submit"><i class="fa fa-trash"></i> Supprimer</button>
                 </form>
+                <!-- ./Delete button -->
                 <?php
                 if (!$task['done']):
                     ?>
+                    <!-- Over button -->
                     <form action="actions.php" method="POST">
                         <input type="hidden" name="action" value="over">
                         <input type="hidden" name="id" value="<?= $task['id'] ?>">
                         <button type="submit"><i class="fa fa-check"></i> Terminé</button>
                     </form>
+                    <!-- ./Over button -->
                     <?php
                 endif;
                 ?>
